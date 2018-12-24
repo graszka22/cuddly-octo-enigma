2594a64ce8f76b880b0eb707a66287ef54c12609
Angelika Serwa <graszka223@gmail.com>
2018-12-25 00:28:07 +0100

    Add Newton-Rapson improvement

--- src/core.c
+++ src/core.c
@@ -3,6 +3,9 @@
 #include <math.h>
 #include <stdlib.h>
 
+const int max_newton_iterations = 5;
+const double eps = 5;
+
 point_t vec_diff(point_t p0, point_t p1) {
     point_t vec = {
         p1.x-p0.x,
@@ -31,11 +34,15 @@ double vec_dot(point_t p0, point_t p1) {
     return p0.x*p1.x+p0.y*p1.y;
 }
 
-double distance(point_t p0, point_t p1) {
+double squared_distance(point_t p0, point_t p1) {
     point_t vec = vec_diff(p0, p1);
     double x = vec.x;
     double y = vec.y;
-    return sqrt(x*x+y*y);
+    return x*x+y*y;
+}
+
+double distance(point_t p0, point_t p1) {
+    return sqrt(squared_distance(p0, p1));
 }
 
 point_t normalize_vector(point_t vector) {
@@ -45,6 +52,25 @@ point_t normalize_vector(point_t vector) {
     point_t ret = {
         x/d, y/d
     };
+    return ret;
+}
+
+point_t evaluate(cubic_bezier_t curve, double t) {
+    double inv = (1-t);
+    double b_poly[] = {
+        inv*inv*inv,
+        3*t*inv*inv,
+        3*t*t*inv,
+        t*t*t,
+    };
+    point_t V[] = {
+        curve.p0, curve.p1, curve.p2, curve.p3
+    };
+    point_t ret = {0, 0};
+    for(int i = 0; i < 4; ++i) {
+        ret.x += b_poly[i]*V[i].x;
+        ret.y += b_poly[i]*V[i].y;
+    }
     return ret;
 }
 
@@ -69,6 +95,64 @@ void get_chord_lengths(int num_of_points, point_t* points, double* lengths) {
     for(int i = 1; i < num_of_points; ++i) {
         current_length += distance(points[i-1], points[i]);
         lengths[i] = current_length/total_length;
+    }
+}
+
+void get_error(int num_of_points, point_t* points, double* chord_lengths, cubic_bezier_t curve,
+               double* error, int* max_error_index) {
+    double max_dist = 0;
+    int index = 0;
+    for(int i = 0; i < num_of_points; ++i) {
+        point_t q = evaluate(curve, chord_lengths[i]);
+        double dist = squared_distance(q, points[i]);
+        if(dist > max_dist) {
+            index = i;
+            max_dist = dist;
+        }
+    }
+    *error = max_dist;
+    *max_error_index = index;
+}
+
+void recalculate_chord_lengths(int num_of_points, point_t* points, double* chord_lengths,
+                               cubic_bezier_t curve) {
+    point_t V[] = {
+        curve.p0, curve.p1, curve.p2, curve.p3
+    };
+    point_t V1[] = {
+        vec_diff(V[0], V[1]),
+        vec_diff(V[1], V[2]),
+        vec_diff(V[2], V[3])
+    };
+    point_t V2[] = {
+        vec_diff(V1[0], V1[1]),
+        vec_diff(V1[1], V1[2])
+    };
+
+    for(int i = 0; i < num_of_points; ++i) {
+        double t = chord_lengths[i];
+        double t2 = t*t;
+        double t3 = t2*t;
+        double it = 1-t;
+        double it2 = it*it;
+        double it3 = it2*it;
+        point_t Q0 = {
+            V[0].x*it3 + 3*V[1].x*t*it2 + 3*V[2].x*t2*it + V[3].x*t3,
+            V[0].y*it3 + 3*V[1].y*t*it2 + 3*V[2].y*t2*it + V[3].y*t3,
+        };
+        point_t Q1 = {
+            3*(V1[0].x*it2 + 2*V1[1].x*t*it + V1[2].x*t2),
+            3*(V1[0].y*it2 + 2*V1[1].y*t*it + V1[2].y*t2),
+        };
+        point_t Q2 = {
+            6*(V2[0].x*it + V2[1].x*t),
+            6*(V2[0].y*it + V2[1].y*t),
+        };
+        point_t P = points[i];
+        double nominator = (Q0.x - P.x)*Q1.x + (Q0.y - P.y)*Q1.y;
+        double denominator = Q1.x*Q1.x + Q1.y*Q1.y + (Q0.x - P.x)*Q2.x + (Q0.y - P.y)*Q2.y;
+        if(denominator == 0.0f) continue;
+        chord_lengths[i] -= nominator / denominator;
     }
 }
 
@@ -140,6 +224,15 @@ cubic_bezier_t fit_bezier(int num_of_points, point_t* points) {
     get_chord_lengths(num_of_points, points, chord_lengths);
 
     cubic_bezier_t bezier = fit_single_bezier(num_of_points, points, chord_lengths, tan1, tan2);
+
+    for(int i = 0; i < max_newton_iterations; ++i) {
+        double error;
+        int index;
+        get_error(num_of_points, points, chord_lengths, bezier, &error, &index);
+        if(error < eps) break;
+        recalculate_chord_lengths(num_of_points, points, chord_lengths, bezier);
+        bezier = fit_single_bezier(num_of_points, points, chord_lengths, tan1, tan2);
+    }
 
     free(chord_lengths);
     return bezier;
