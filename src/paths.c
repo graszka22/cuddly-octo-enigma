2ff175561892171566f17698f83a3f8b6fe05394
Angelika Serwa <graszka223@gmail.com>
2018-12-28 16:59:22 +0100

    Detect paths

--- src/paths.c
+++ src/paths.c
@@ -1,5 +1,7 @@
 #include "paths.h"
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "draw.h"
 
 void destroy_path(path_t path) {
@@ -54,9 +56,7 @@ uint8_t* detect_junctions(uint8_t* image, int width, int height) {
     return junctions;
 }
 
-uint8_t* detect_endpoints(uint8_t* image, int width, int height) {
-    uint8_t* endpoints = malloc(width*height);
-    memset(endpoints, 0, width*height);
+void detect_endpoints(uint8_t* image, uint8_t* junctions, int width, int height) {
     int8_t masks[8][3][3] = {
         {{0, 1, 0}, {-1, 1, -1}, {-1, -1, -1}},
         {{1, -1, -1}, {-1, 1, -1}, {-1, -1, -1}},
@@ -67,18 +67,67 @@ uint8_t* detect_endpoints(uint8_t* image, int width, int height) {
         {{-1, -1, 0}, {-1, 1, 1}, {-1, -1, 0}},
         {{-1, -1, 1}, {-1, 1, -1}, {-1, -1, -1}}
     };
-    apply_mask(image, endpoints, masks, 8, width, height, 1, 2);
-    return endpoints;
+    apply_mask(image, junctions, masks, 8, width, height, 1, 2);
+}
+
+path_t* detect_paths(uint8_t* image, uint8_t* junctions, int width, int height, int* number_of_paths) {
+    point_t* current_path = malloc(width*height*sizeof(point_t));
+    path_t* paths = malloc(width*height*sizeof(path_t));
+    int k = 0, n = 0;
+    for(int y = 0; y < height; ++y)
+    for(int x = 0; x < width; ++x) {
+        if(!junctions[y*width+x]) continue;
+        while(1) {
+            k = 0;
+            point_t c = {x, y};
+            current_path[k++] = c;
+            int xx = x, yy = y;
+            while(1) {
+                bool end_of_path = true;
+                for(int i = -1; i < 2; ++i)
+                for(int j = -1; j < 2; ++j) {
+                    if(i == 0 && j == 0) continue;
+                    if(xx+j < 0 || xx+j >= width || yy+i < 0 || yy+i >= height) continue;
+                    if(image[(yy+i)*width+xx+j]) {
+                        end_of_path = false;
+                        c.x = xx+j;
+                        c.y = yy+i;
+                        current_path[k++] = c;
+                        if(junctions[(yy+i)*width+xx+j])
+                            end_of_path = true;
+                        image[(yy+i)*width+xx+j] = 0;
+                        xx += j;
+                        yy += i;
+                        goto BREAK_LOOP;
+                    }
+                }
+                BREAK_LOOP:
+                if(end_of_path)
+                    break;
+            }
+            if(k == 1) break;
+            if(k > 5) {
+                point_t* new_path = malloc(k*sizeof(point_t));
+                memcpy(new_path, current_path, sizeof(point_t)*k);
+                path_t path = {
+                    .points_count = k,
+                    .points = new_path
+                };
+                paths[n++] = path;
+            }
+        }
+    }
+    free(current_path);
+    *number_of_paths = n;
+    //TODO: closed paths!
+    return paths;
 }
 
 path_t* get_paths(uint8_t* image, int width, int height, int* number_of_paths) {
     make_8_connect(image, width, height);
     uint8_t* junctions = detect_junctions(image, width, height);
-    uint8_t* endpoints = detect_endpoints(image, width, height);
-    save_grayscale(image, endpoints, width, height);
-    path_t* paths = malloc(1);
-    *number_of_paths = 0;
+    detect_endpoints(image, junctions, width, height);
+    path_t* paths = detect_paths(image, junctions, width, height, number_of_paths);
     free(junctions);
-    free(endpoints);
     return paths;
 }
