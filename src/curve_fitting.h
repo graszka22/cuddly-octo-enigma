65ce46bf5fec404d6565872146eb6d8855bad30a
Angelika Serwa <graszka223@gmail.com>
2019-01-19 21:03:16 +0100

    Remove dead code

--- src/curve_fitting.h
+++ src/curve_fitting.h
@@ -4,6 +4,5 @@
 #include "defs.h"
 
 cubic_bezier_t* fit_bezier(int num_of_points, point_t* points, int* num_of_beziers);
-void get_bezier_points(int num_of_points, cubic_bezier_t bezier, point_t* points);
 
 #endif
