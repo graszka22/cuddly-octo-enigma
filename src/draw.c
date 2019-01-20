9fe26d732fd3a51eceb1b7ae92a5137cbfcdc53e
Angelika Serwa <graszka223@gmail.com>
2019-01-20 23:10:36 +0100

    fix

--- src/draw.c
+++ src/draw.c
@@ -63,7 +63,7 @@ void destroy_image_data(image_data_t image_data) {
 
 void draw_bezier(image_t image, cubic_bezier_t bezier) {
     cairo_set_source_rgba(image.cr, 0, 0, 0, 1);
-    cairo_set_line_width(image.cr, 2);
+    cairo_set_line_width(image.cr, program_options.line_width);
     cairo_move_to(image.cr, bezier.p0.x, bezier.p0.y);
     cairo_curve_to(image.cr,
                    bezier.p1.x, bezier.p1.y,
